e91fa0cc7d61898f84639d61fbc942487476dec1
Dafin1723 <dafinrifat@gmail.com>
2026-04-08 14:12:26 +0700

    add code

--- bot_laundry.py
+++ bot_laundry.py
@@ -1,36 +1,7 @@
-import logging
-import sqlite3
-import random
-from datetime import datetime
-
-from telegram import (
-    Update,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    ReplyKeyboardMarkup,
-    KeyboardButton
-)
-
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    CallbackQueryHandler,
-    MessageHandler,
-    Filters,
-    CallbackContext
-)
-
-# ==============================
-# CONFIG
-# ==============================
-
-TOKEN = "8729351085:AAEa2yXbwnCGccagbAZqSpV7LkIc12KJ_7E"
-ADMIN_ID = 8028474070
-
 logging.basicConfig(level=logging.INFO)
 
 # ==============================
-# DATABASE
+# DATABASE 1
 # ==============================
 
 conn = sqlite3.connect("laundry.db", check_same_thread=False)
