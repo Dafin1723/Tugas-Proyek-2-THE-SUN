cfcde7d5625f8364255079997fa1d1f5ec59a446
dapinn1723 <dafinrifat@gmail.com>
2026-04-11 09:32:47 +0700

    add code

--- src/bot_laundry.py
+++ src/bot_laundry.py
@@ -1,3 +1,32 @@
+import logging
+import sqlite3
+import random
+from datetime import datetime
+
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext
+)
+
+# ==============================
+# CONFIG
+# ==============================
+
+TOKEN = "ISI_PAKE_TOKEN_BOT"
+ADMIN_ID = 8028474070
+
 logging.basicConfig(level=logging.INFO)
 
 # ==============================
